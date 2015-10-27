1df200ba240f0096d21410c52c0d5843cd3d2912
Alexander Hughes <ahughes1231@gmail.com>
2015-10-27 11:53:53 -0400

    Enforce post URL uniqueness

--- collections/posts.js
+++ collections/posts.js
@@ -1,7 +1,31 @@
-Posts = new Meteor.Collection('posts');
+Posts = new Mongo.Collection('posts');
 
-Posts.allow({
-  insert: function(userId, doc) {
-    return !! userId;
+Meteor.methods({
+  PostInsert: function(postAttributes) {
+    check(this.userId(), String);
+    check(postAttributes, {
+      title: String,
+      url: String
+    });
+    
+    var postWithSameLink = Posts.findOne({url: postAtributes.url});
+    if (postWithSameLink) {
+      return {
+        postExists: true, _id: postWithSameLink._id
+      }
+    }
+    
+    var user = Meteor.user();
+    var post = _.extend(postAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date()
+    });
+    
+    var PostId = Posts.insert(post);
+    
+    return {
+      _id: postId
+    };
   }
 });
